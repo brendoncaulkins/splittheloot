bc9361975c439a4eba1c6883773f916e6409f787
brendon.caulkins <brendon.caulkins@gmail.com>
2020-08-14 00:36:59 -0400

    Reduce App component footprint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <p>It works!</p> `,
 })
-export class AppComponent {
-  title = 'splittheloot';
-}
+export class AppComponent {}
