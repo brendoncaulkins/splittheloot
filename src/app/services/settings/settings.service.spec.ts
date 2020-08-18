4aa1bdb9c990b64733f5ea99c5620c710f4e149f
brendon.caulkins <brendon.caulkins@gmail.com>
2020-08-17 21:35:34 -0400

    Achieve 100% Code Coverage

--- src/app/services/settings/settings.service.spec.ts
+++ src/app/services/settings/settings.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing'
+import { filter } from 'rxjs/operators'
 
 import { SettingsService } from './settings.service'
 
@@ -12,5 +13,31 @@ describe('SettingsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  describe('current', () => {
+    it('should broadcast the current settings to new subscribers', (done) => {
+      const settings = { partyPercentage: 30, partySize: 4, convertToGold: true }
+      service.updateSettings(settings)
+
+      service.current$.subscribe((data) => {
+        expect(data).toEqual(settings)
+        done()
+      })
+    })
+  })
+
+  describe('updateSettings', () => {
+    it('should cache the new settings data and bradcast via .current', (done) => {
+      const settings = { partyPercentage: 30, partySize: 4, convertToGold: true }
+
+      // Filter out the initial data broadcast of default data on subscribe
+      service.current$.pipe(filter((data) => data.convertToGold)).subscribe((data) => {
+        expect(data).toEqual(settings)
+        done()
+      })
+
+      service.updateSettings(settings)
+    })
   })
 })
--- src/app/settings-form/settings-form.component.spec.ts
+++ src/app/settings-form/settings-form.component.spec.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
 import { MaterialModule } from '../material.module'
+import { initialSettings } from '../models/settings.model'
 import { SettingsService } from '../services/settings/settings.service'
 import { SettingsFormComponent } from './settings-form.component'
 
@@ -26,5 +27,33 @@ describe('SettingsFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  describe('form validations', () => {
+    it('should have partyPercentage, partySize, and convertToGold controls', () => {
+      expect(component.formGroup.contains('partyPercentage')).toBe(true)
+      expect(component.formGroup.contains('partySize')).toBe(true)
+      expect(component.formGroup.contains('convertToGold')).toBe(true)
+    })
+    it('should initialize correctly', () => {
+      expect(component.formGroup.value).toEqual(initialSettings)
+    })
+    it('should not allow negative values', () => {
+      component.formGroup.patchValue({
+        partyPercentage: -1,
+        partySize: -1,
+        convertToGold: false,
+      })
+      expect(component.hasError('partyPercentage', 'min')).toBe(true)
+      expect(component.hasError('partySize', 'min')).toBe(true)
+    })
+    it('should not allow a partyPercentage over 100%', () => {
+      component.formGroup.get('partyPercentage').setValue(101)
+      expect(component.hasError('partyPercentage', 'max')).toBe(true)
+    })
+    it('should not allow a partySize over 10', () => {
+      component.formGroup.get('partySize').setValue(11)
+      expect(component.hasError('partySize', 'max')).toBe(true)
+    })
   })
 })
